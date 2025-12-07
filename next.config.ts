ad76917c6a59b98246eac6995bc5c951b8554678
Ammaar <ammaaryeah@gmail.com>
2025-12-07 08:30:19 +0000

    13:58:45.715 Error occurred prerendering page "/_not-found". Read more:

--- next.config.ts
+++ next.config.ts
@@ -30,7 +30,8 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https'
+        ,
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
@@ -55,10 +56,6 @@ const nextConfig: NextConfig = {
       }
     ],
   },
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  }
 };
 
 export default nextConfig;
