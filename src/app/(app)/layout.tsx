88506ce2b7c80abfc9748359f313ad12de930e28
Ammaar <ammaaryeah@gmail.com>
2025-12-07 07:28:17 +0000

    the chimney is bleeding, can u raise it up a few pixels

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -30,7 +30,7 @@ const HomeIconWithChimney = (props: React.SVGProps<SVGSVGElement>) => (
     >
         <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
         <polyline points="9 22 9 12 15 12 15 22"/>
-        <path d="M19 9.5V5h-2v2.5"/>
+        <path d="M19 9.5V4h-2v3.5"/>
     </svg>
 )
 
