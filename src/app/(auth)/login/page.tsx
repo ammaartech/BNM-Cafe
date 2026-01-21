d35011db12069e2dd2fb7156a84584ecc81dd879
Ammaar <ammaaryeah@gmail.com>
2026-01-21 13:44:49 +0000

    discarded workflow code, database is still updated with order flow but deleted the pages

--- src/app/(auth)/login/page.tsx
+++
@@ -1,3 +0,0 @@
-// This file is obsolete and has been deactivated to prevent route conflicts.
-// The primary customer login is now at /src/app/login/page.tsx.
-export {};
--- src/app/(staff)/login/page.tsx
+++
@@ -1,2 +0,0 @@
-// This file is obsolete and has been deactivated to prevent route conflicts.
-export {};
